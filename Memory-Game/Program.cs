ff5f37805185cab71ff42b6adb7b37de5b10ae36
Drozdzu333 <drozdzu333@gmail.com>
2022-02-22 18:15:03 +0100

    Dodaj pliki projektów.

---
+++ Memory-Game/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MemoryGame
+{
+
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.Clear();
+            Board(5);
+        }
+
+        static void Board(int col)
+        {
+            //Header coordinates
+            Console.Write("  ");
+            for (int i = 1; i <= col; i++)
+            {
+                Console.Write("  " + i + " ");
+            }
+            Console.Write("\n");
+            //Side coordinates and play-board
+            Console.Write("A ");
+            for (int i = 0; i < col; i++)
+            {
+                Console.Write("; X ");
+            }
+            Console.Write("\n");
+            Console.Write("B ");
+            for (int i = 0; i < col; i++)
+            {
+                Console.Write("; X ");
+            }
+        }
+    }
+}
