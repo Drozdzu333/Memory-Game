3f5301101ae389a128ed80683fafbe158fc687cf
Drozdzu333 <drozdzu333@gmail.com>
2022-02-23 13:47:32 +0100

    Added class Board

---
+++ Memory-Game/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory_Game
+{
+    internal class Board 
+    {
+        int lenght;
+        string[] words;
+        public Board(int eLength, string[] eWords)
+        {
+            lenght = eLength;
+            words = eWords;
+        }
+        public void PrintBoard()
+        {
+            //Header coordinates
+            Console.Write("  ");
+            for (int i = 1; i <= lenght; i++)
+            {
+                Console.Write("  " + i + " ");
+            }
+            Console.Write("\n");
+
+            //Side coordinates and play-board
+            Console.Write("A ");
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write("; X ");
+            }
+            Console.Write("\n");
+            Console.Write("B ");
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write("; X ");
+            }
+            Console.Write("\n");
+            Console.Write("C ");
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write($"; {words[i]} ");
+            }
+        }
+    }
+}
--- Memory-Game/Program.cs
+++ Memory-Game/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MemoryGame
+﻿using Memory_Game;
+
+namespace MemoryGame
 {
 
     class Program
@@ -94,7 +96,9 @@
 
 
                 // End of level loop
-            Board(5);
+            
+            Board board = new Board(boardLength, words);
+                board.PrintBoard();
             exit = Console.ReadLine() == "exit" ? true: false ;
             }
             // end of Main Loop
@@ -104,28 +108,6 @@
 
 
 
-        static void Board(int col)
-        {
-            //Header coordinates
-            Console.Write("  ");
-            for (int i = 1; i <= col; i++)
-            {
-                Console.Write("  " + i + " ");
-            }
-            Console.Write("\n");
- 
-            //Side coordinates and play-board
-            Console.Write("A ");
-            for (int i = 0; i < col; i++)
-            {
-                Console.Write("; X ");
-            }
-            Console.Write("\n");
-            Console.Write("B ");
-            for (int i = 0; i < col; i++)
-            {
-                Console.Write("; X ");
-            }
-        }
+       
     }
 }
