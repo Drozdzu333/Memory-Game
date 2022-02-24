993ab0e1a67d83f26536d0f5a101461da82b4d11
Drozdzu333 <drozdzu333@gmail.com>
2022-02-25 00:13:37 +0100

    Added ScoreBoard with functionality

---
+++ Memory-Game/ScoreBoard.cs
@@ -0,0 +1,17 @@
+﻿
+//namespace Memory_Game
+//{
+//    public class ScoreBoard
+//    {
+//        //
+//        private string[,] scoreList = new string[10,2];
+
+//        public string[,] ScoreList
+//        { get => scoreList;
+//          set =>{
+//                if(value >= Array. scoreList)
+//            };
+
+//        }
+//    }
+//}
